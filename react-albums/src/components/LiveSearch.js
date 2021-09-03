8a16e58b8fd419063c3bcb596964a0ede4a313b1
NahomKibreab <nknahom@gmail.com>
2021-09-02 21:25:14 -0600

    Add axios to fetch data

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,19 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
+import axios from "axios";
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const testURL = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    axios.get(testURL).then((response) => {
+      console.log(setResults([...response.data.results]));
+    });
+  }, [term]);
 
   return (
     <Fragment>
@@ -13,7 +21,7 @@ export default function LiveSearch(props) {
         <img src="images/brand.png" alt="Brand" />
       </header>
       <main>
-        <SearchBar onSearch={term => setTerm(term)} />
+        <SearchBar onSearch={(term) => setTerm(term)} />
         <Results results={results} />
       </main>
     </Fragment>
